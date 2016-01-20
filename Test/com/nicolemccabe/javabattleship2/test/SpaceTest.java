872a9f67fa58e8117b5633ae37ffcf0bb55f7643
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-01-20 14:20:24 -0500

    Space inits without a ship
    Space inits with a boolean var hasShip set to false

--- Test/com/nicolemccabe/javabattleship2/test/SpaceTest.java
+++ Test/com/nicolemccabe/javabattleship2/test/SpaceTest.java
@@ -5,6 +5,7 @@ import com.nicolemccabe.javabattleship2.State;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.TestCase.assertFalse;
 
 public class SpaceTest {
     @Test
@@ -18,5 +19,11 @@ public class SpaceTest {
         Space s = new Space();
         s.logStrike();
         assertEquals(State.MISS, s.getState());
+    }
+
+    @Test
+    public void testSpaceInitsWithoutShip() {
+        Space s = new Space();
+        assertFalse(s.hasShip());
     }
 }
--- src/com/nicolemccabe/javabattleship2/Space.java
+++ src/com/nicolemccabe/javabattleship2/Space.java
@@ -2,13 +2,19 @@ package com.nicolemccabe.javabattleship2;
 
 public class Space {
     private State state;
+    private boolean hasShip;
 
     public Space() {
         this.state = State.WATER;
+        this.hasShip = false;
     }
 
     public State getState() {
         return state;
+    }
+
+    public boolean hasShip() {
+        return hasShip;
     }
 
     public void logStrike() {
