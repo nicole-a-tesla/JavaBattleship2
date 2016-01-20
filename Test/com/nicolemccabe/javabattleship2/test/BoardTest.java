57836ccbcaf1ccf22f351bcce2826db3496edc02
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-01-20 11:31:56 -0500

    Board builds 10x10 internal grid
    * buildGrid() method
    * buildRow() method
    * tests

--- Test/com/nicolemccabe/javabattleship2/test/BoardTest.java
+++ Test/com/nicolemccabe/javabattleship2/test/BoardTest.java
@@ -1,16 +1,29 @@
 package com.nicolemccabe.javabattleship2.test;
 
 import com.nicolemccabe.javabattleship2.Board;
+import com.nicolemccabe.javabattleship2.Space;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static junit.framework.Assert.assertEquals;
 
 public class BoardTest {
+    Board b = new Board(10);
+
     @Test
-    public void calculatesSize() {
-        Board b = new Board(1);
-        assertEquals(1, b.size);
+    public void hasSize() {
+        assertEquals(10, b.size);
         b = new Board(7);
         assertEquals(7, b.size);
     }
+
+    @Test
+    public void creates10x10grid() {
+        int rowSize = b.getGrid().size();
+        assertEquals(10 , rowSize);
+        ArrayList col = (ArrayList) b.getGrid().get(0);
+        assertEquals(10 , col.size());
+    }
+
 }
--- src/com/nicolemccabe/javabattleship2/Board.java
+++ src/com/nicolemccabe/javabattleship2/Board.java
@@ -1,10 +1,36 @@
 package com.nicolemccabe.javabattleship2;
 
+import java.util.ArrayList;
+
 public class Board {
     public int size;
+    private ArrayList grid;
 
     public Board(int size) {
         this.size = size;
+        this.grid = buildGrid();
+    }
+
+    public ArrayList getGrid() {
+        return grid;
+    }
+
+    private ArrayList buildGrid() {
+        ArrayList localGrid = new ArrayList();
+
+        for (int i=0;i<size;i++) {
+            localGrid.add(buildRow());
+        }
+        return localGrid;
+    }
+
+    private ArrayList buildRow() {
+        ArrayList row = new ArrayList();
+
+        for (int i = 0; i < size; i++)
+            row.add(new Space());
+
+        return row;
     }
 
 }
