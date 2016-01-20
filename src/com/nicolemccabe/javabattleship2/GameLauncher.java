395c4e010449c055f89045b3648e5cfb8e4c0384
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-01-20 10:36:12 -0500

    Change board in GameLauncher to print 10x10 board
    It no longer makes sense to print a 1x1 board, so changed launchers
    print statement
    * changed GameLauncher print statement to print Board(10), not Board(1)

--- src/com/nicolemccabe/javabattleship2/GameLauncher.java
+++ src/com/nicolemccabe/javabattleship2/GameLauncher.java
@@ -4,10 +4,9 @@ import java.io.IOException;
 
 public class GameLauncher {
 
-
     public static void main(String[] args) throws IOException {
         BoardPrinter printer = new BoardPrinter();
-        Board board = new Board(1);
+        Board board = new Board(10);
         printer.print(board);
 
 //        Game game = new Game();
