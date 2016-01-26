89add6c2b12b905769f0655fb89adbb2eb87f3ac
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-01-26 11:01:24 -0500

    Fix depricated library compiler warning

--- Test/com/nicolemccabe/javabattleship2/test/GameTest.java
+++ Test/com/nicolemccabe/javabattleship2/test/GameTest.java
@@ -10,9 +10,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
