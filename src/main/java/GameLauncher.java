ae97dfc3dce6dbb7c3dd6a55c82a6da4b8270693
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-02-02 10:34:12 -0500

    Remove more uneccessray declaration from GameLauncher

--- src/main/java/GameLauncher.java
+++ src/main/java/GameLauncher.java
@@ -5,14 +5,10 @@ import java.io.IOException;
 public class GameLauncher {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        Board board = new Board(10);
-        BoardFormatter formatter = new BoardFormatter();
         Printer consolePrinter = new ConsolePrinter();
-        BoardPrinter boardPrinter = new BoardPrinter(consolePrinter);
-        BoardPrintManager manager = new BoardPrintManager(formatter, boardPrinter);
-
+        BoardPrintManager manager = new BoardPrintManager(new BoardFormatter(), new BoardPrinter(consolePrinter));
         Ui ui = new Ui(consolePrinter, new ConsoleReceiver(), manager);
-        Game game = new Game(board, ui);
+        Game game = new Game(new Board(10), ui);
 
         game.startGame();
 
